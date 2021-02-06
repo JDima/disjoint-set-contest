d0bc81b06ccd0bca8c40ac55af32a5b5c8434981
Ilia Kuzmin <kyzmin.ilia@yandex.ru>
2021-02-06 20:50:02 +0300

    added tests for third task

--- tasks/task3/test_public.py
+++ tasks/task3/test_public.py
@@ -22,22 +22,58 @@ TEST_CASES = [
         cost=1,
     ),
     Case(
-        name='base1',
+        name='base2',
         k=3,
         array=[1, 2, 3, 4],
         cost=2,
     ),
     Case(
-        name='base1',
+        name='base3',
         k=3,
         array=[5, 3, 4, 2, 6],
         cost=2,
     ),
     Case(
-        name='base1',
+        name='base4',
         k=4,
         array=[5, 3, 50, 2, 4, 5],
         cost=3,
+    ),
+    Case(
+        name='base5',
+        k=1,
+        array=[3, 5],
+        cost=3,
+    ),
+    Case(
+        name='base6',
+        k=1,
+        array=[5, 3],
+        cost=3,
+    ),
+    Case(
+        name='base7',
+        k=2,
+        array=[5, 3],
+        cost=3,
+    ),
+    Case(
+        name='base8',
+        k=5,
+        array=[4, 7, 9, 1, 1, 5, 10, 6],
+        cost=5,
+    ),
+    Case(
+        name='base9',
+        k=1,
+        array=[7],
+        cost=7,
+    ),
+    Case(
+        name='base10',
+        k=6,
+        array=[4, 7, 9, 1, 1, 5, 10, 6, 90, 50, 1, 2],
+        cost=4,
     ),
 ]
 
