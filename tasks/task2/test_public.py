24c1ec3a2c71c68833ca4cefb217ae9ed74b298e
Ilia Kuzmin <kyzmin.ilia@yandex.ru>
2021-02-06 20:44:39 +0300

    added tests for second task

--- tasks/task2/test_public.py
+++ tasks/task2/test_public.py
@@ -15,9 +15,34 @@ class Case:
 
 TEST_CASES = [
     Case(
-        name='base',
+        name='base1',
         pandas=[1, 2, 3, 4, 5, 4, 3, 2, 1, 6],
         powers=[6, 4, 4, 3, 3, 2, 2, 1, 1, 1],
+    ),
+    Case(
+        name='base2',
+        pandas=[1, 2, 3],
+        powers=[3, 2, 1],
+    ),
+    Case(
+        name='base3',
+        pandas=[7, 9, 13, 6],
+        powers=[13, 9, 7, 6],
+    ),
+    Case(
+        name='base4',
+        pandas=[1, 3, 5, 2, 4],
+        powers=[5, 3, 2, 2, 1]
+    ),
+    Case(
+        name='base5',
+        pandas=[3, 2, 8, 4, 5, 4, 3, 5, 1, 1, 9],
+        powers=[9, 4, 4, 4, 3, 3, 2, 2, 1, 1, 1],
+    ),
+    Case(
+        name='base6',
+        pandas=[5],
+        powers=[5]
     ),
 ]
 
